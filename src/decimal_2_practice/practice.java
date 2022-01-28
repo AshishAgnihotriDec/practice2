f0ddb340534a7ad573c11b6d4b9b60509671b708
AshishAgnihotriDec <ashish.agnihotri@decimal.co.in>
2022-01-28 15:56:03 +0530

    test message

---
+++ src/decimal_2_practice/practice.java
@@ -0,0 +1,11 @@
+package decimal_2_practice;
+
+public class practice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+		
+	}
+
+}
